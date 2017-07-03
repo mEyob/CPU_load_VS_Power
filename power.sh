70f32101b64af06a47cfa8ce8ebfcf724f105811
Misikir <mikireyob@gmail.com>
2017-07-03 22:51:36 +0300

    Initial commit

---
+++ power.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+Voltage=$(/usr/sbin/system_profiler SPPowerDataType | grep 'Voltage (mV)' | egrep -o '[0-9]+')
+Amperage=$(/usr/sbin/system_profiler SPPowerDataType | grep 'Amperage (mA)' | egrep -o '[\-]*[0-9]+')
+t=$(date -j -f "%a %b %d %T %Z %Y" "`date`" "+%s")
+
+printf '%s\t%s\t%s\n' "$t" "$Voltage" "$Amperage" >> volt-amp.txt
