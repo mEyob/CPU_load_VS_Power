34200b359fb875c7c34250e36a5c4bbc8eb90ff7
Misikir <mikireyob@gmail.com>
2017-07-06 23:22:42 +0300

    larger samples

--- power.sh
+++ power.sh
@@ -2,6 +2,8 @@
 
 Voltage=$(/usr/sbin/system_profiler SPPowerDataType | grep 'Voltage (mV)' | egrep -o '[0-9]+')
 Amperage=$(/usr/sbin/system_profiler SPPowerDataType | grep 'Amperage (mA)' | egrep -o '[\-]*[0-9]+')
+RemCharge=$(/usr/sbin/system_profiler SPPowerDataType | grep 'Charge Remaining (mAh)'| egrep -o '[0-9]+')
+
 t=$(date -j -f "%a %b %d %T %Z %Y" "`date`" "+%s")
 
-printf '%s\t%s\t%s\n' "$t" "$Voltage" "$Amperage" >> volt-amp.txt
+printf '%s\t%s\t%s\t%s\n' "$t" "$Voltage" "$Amperage" "$RemCharge" >> volt-amp.txt
